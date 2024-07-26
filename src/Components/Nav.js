f8151bbd2f0bba9640e0980fff27d83290014917
YASHRAJ DHAMALE <yashrajdhamale15@gmail.com>
2024-07-26 15:09:25 +0530

    first commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,48 @@
+import './Css/Nav.css'; // Correct import statement based on your project structure
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faMicrophone, faVideoSlash, faBell, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import logo from './Images/logo.svg';
+
+export default function Nav() {
+  const [ChangeID, setChangeID] = useState(false);
+
+  const handleLeftSearch = () => {
+    setChangeID(true);
+  }
+
+  const dontShowLeftSearch = () => {
+    setChangeID(false);
+  }
+
+  return (
+    <div id="nav">
+      <div id="left">
+        <FontAwesomeIcon icon={faBars} id='baricon' />
+        <img src={logo} alt="youtube" id='logoimg' />
+      </div>
+
+      <div id="midpart">
+        <div id={ChangeID ? 'search-left' : 'dontshow-searchleft'}>
+          <FontAwesomeIcon icon={faSearch} id='searchicon' />
+        </div>
+        <div id="input">
+          <input type="text" placeholder="Search" id={!ChangeID ? 'textinput' : 'textinputchange'} onClick={handleLeftSearch} onBlur={dontShowLeftSearch} />
+        </div>
+        <div id="search">
+          <FontAwesomeIcon icon={faSearch} id='searchicon' />
+        </div>
+        <div id="mic">
+          <FontAwesomeIcon icon={faMicrophone} id='micicon' />
+        </div>
+      </div>
+
+      <div id="right">
+        <FontAwesomeIcon icon={faVideoSlash} className='icon' />
+        <FontAwesomeIcon icon={faBell} className='icon' />
+        <FontAwesomeIcon icon={faUser} className='icon' />
+      </div>
+    </div>
+  );
+}
